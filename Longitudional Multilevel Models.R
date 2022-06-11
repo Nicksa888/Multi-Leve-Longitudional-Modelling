37e0a58016f12301657ec868f2374a02c2d54361
Nicksa888 <84227001+Nicksa888@users.noreply.github.com>
2022-06-11 15:39:00 +0100

    Update Longitudional Multilevel Models.R

--- Longitudional Multilevel Models.R
+++ Longitudional Multilevel Models.R
@@ -13,9 +13,18 @@ library(tidyverse)
 library(dplyr)
 library(lme4)
 
-# With longitudinal models, the data structure takes the following form: repeated measurements (level 1) nested within the individual (level 2), and possibly individual nested within some higher-level cluster (e.g. school) at level 3. Compared to regular multilevel modelling, the primary difference is that now we have three different types of predictors: a time predictor, time-varying predictors, and time-invariant predictors. Given their unique role in longitudinal modeling, it is worth spending just a bit of time defining each of these predictor types.
+# With longitudinal models, the data structure takes the following form: repeated measurements (level 1) nested within the individual (level 2), and possibly 
+# individual nested within some higher-level cluster (e.g. school) at level 3. Compared to regular multilevel modelling, the primary difference is that now we have 
+# three different types of predictors: a time predictor, time-varying predictors, and time-invariant predictors. Given their unique role in longitudinal modeling, it 
+# is worth spending just a bit of time defining each of these predictor types.
 
-# The data to be used in the following examples come from the realm of educational testing. Examinees were given a language assessment at six equally spaced times. As always, the data must first be read into R. In this case, the data are in the person-level data structure, in a file called Lang. This file includes the total language achievement test score measured over six different measurement occasions (the outcome variable), four language subtest scores (writing process and features, writing applications, grammar, and goals), and variables indicating student ID and school ID. Restructuring person-level data into person period format in R can be accomplished by creating a new data frame from the person-level data using the stack command. All time-invariant variables will need to be copied into the new data file, while time-variant variables (e.g. all test scores measured over the six measurement occasions) will need to be stacked in order to create person period format. The following R commands will read the Lang file into R and then rearrange the data into the necessary format
+# The data to be used in the following examples come from the realm of educational testing. Examinees were given a language assessment at six equally spaced times. As 
+# always, the data must first be read into R. In this case, the data are in the person-level data structure, in a file called Lang. This file includes the total 
+# language achievement test score measured over six different measurement occasions (the outcome variable), four language subtest scores (writing process and 
+# features, writing applications, grammar, and goals), and variables indicating student ID and school ID. Restructuring person-level data into person period format 
+# in R can be accomplished by creating a new data frame from the person-level data using the stack command. All time-invariant variables will need to be copied into
+# the new data file, while time-variant variables (e.g. all test scores measured over the six measurement occasions) will need to be stacked in order to create 
+# person period format. The following R commands will read the Lang file into R and then rearrange the data into the necessary format
 
 # Load Data ---------------------------------------------------------------
 
@@ -37,7 +46,8 @@ glimpse(LangPP)
 names(LangPP)[7] <- "Language"
 names(LangPP)
 
-# We may also wish to recode the dedicated time variable, ind. Currently, this  variable is not recorded numerically, but takes on the values “LangScore1,” “LangScore2,” “LangScore3,” “LangScore4,” “LangScore5,” “LangScore6”. Thus we may wish to recode the values to make a continuous numeric time predictor, as follows:
+# We may also wish to recode the dedicated time variable, ind. Currently, this  variable is not recorded numerically, but takes on the values “LangScore1,” 
+# “LangScore2,” “LangScore3,” “LangScore4,” “LangScore5,” “LangScore6”. Thus we may wish to recode the values to make a continuous numeric time predictor, as follows:
 
 LangPP <- LangPP %>% mutate(Time = recode(ind, 
                                  "LangScore1" = '0',
@@ -87,9 +97,12 @@ Time3 -0.303  0.500  0.500
 Time4 -0.303  0.500  0.500  0.500       
 Time5 -0.303  0.500  0.500  0.500  0.500
 
-# It is important to point out that these results indicate a statistically significant positive relationship between time and performance on the language assessment (t=99.45), such that scores increased over time. Also, the confidence intervals for the fixed and random effects all exclude 0, indicating that they are different from 0 in the population, i.e. statistically significant.
+# It is important to point out that these results indicate a statistically significant positive relationship between time and performance on the language assessment 
+# (t=99.45), such that scores increased over time. Also, the confidence intervals for the fixed and random effects all exclude 0, indicating that they are different 
+# from 0 in the population, i.e. statistically significant.
 
-# Adding predictors to the model also remains the same as in earlier examples, regardless of whether they are time varying or time invariant. For example, in order to add Grammar, which is time varying, as a predictor of total language scores over time, we would use the following:
+# Adding predictors to the model also remains the same as in earlier examples, regardless of whether they are time varying or time invariant. For example, in order 
+# to add Grammar, which is time varying, as a predictor of total language scores over time, we would use the following:
 
 Model1.1 <- lmer(Language ~ Time + Grammar +(1|ID), LangPP)
 
@@ -130,9 +143,12 @@ Time4   -0.085  0.500  0.500  0.500
 Time5   -0.085  0.500  0.500  0.500  0.500       
 Grammar -0.988  0.000  0.000  0.000  0.000  0.000
 
-# From these results, we see that, again, Time is positively related to scores on the language assessment, indicating that they increased over time. In addition, Grammar is also statistically significantly related to language test scores (t=114.23), meaning that higher grammar test scores were associated with higher language scores. 
+# From these results, we see that, again, Time is positively related to scores on the language assessment, indicating that they increased over time. In addition, 
+# Grammar is also statistically significantly related to language test scores (t=114.23), meaning that higher grammar test scores were associated with higher 
+# language scores. 
 
-# We could add a third level of data structure to this model by including information regarding schools, within which examinees are nested. To fit this model we use the following code in R:
+# We could add a third level of data structure to this model by including information regarding schools, within which examinees are nested. To fit this model we 
+# use the following code in R:
 
 Model1.3 <- lmer(Language ~ Time + (1|school/ID), 
                  data = LangPP)
@@ -187,4 +203,5 @@ Model1.3    9 133425 133496 -66704   133407 521.99  1  < 2.2e-16 ***
   ---
   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
 
-# Given that the AIC for Model1.3 is lower than that for Model1.0 where school is not included as a variable, and the chi-square test for deviance is significant (χ2=521.99, p<.001), we can conclude that inclusion of the school level of the data leads to better model fit.
+# Given that the AIC for Model1.3 is lower than that for Model1.0 where school is not included as a variable, and the chi-square test for deviance is significant 
+# (χ2=521.99, p<.001), we can conclude that inclusion of the school level of the data leads to better model fit.
